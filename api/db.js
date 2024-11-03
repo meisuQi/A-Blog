2b603aa921d4dc5ee3bcc3cac5ef0bfe6bcd58ca
meisuQi <2235889780@qq.com>
2024-11-03 19:57:18 +0800

    deleted

--- api/db.js
+++ api/db.js
@@ -1,37 +1,31 @@
-// import mysql from 'mysql';
-// export const db=mysql.createConnection({
-//   host:'localhost',
-//   user:"root",
-//   password:"qms199812230",
-//   database:"blog"
-// })
+import mysql from 'mysql';
+export const db=mysql.createConnection({
+  host:'sql12.freesqldatabase.com',
+  user:"sql12742423",
+  password:"jVuwEP3kPW",
+  database:"sql12742423",
+  port:3306
+})
 
-// // 连接到数据库
-// db.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to MySQL:', err);
-//     return;
-//   }
-//   console.log('Connected to MySQL database');
-// });
-
-// // 监听数据库连接错误
-// db.on('error', (err) => {
-//   console.error('MySQL error occurred:', err);
-//   if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-//     console.error('Database connection was closed.');
-//   } else {
-//     console.error('Unexpected error:', err.code);
-//   }
-// });
-
-// // 导出数据库连接对象
-// export default db;
-const db = require('pg');
-const pool = new db.Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 5432,
+// 连接到数据库
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
 });
+
+// 监听数据库连接错误
+db.on('error', (err) => {
+  console.error('MySQL error occurred:', err);
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    console.error('Database connection was closed.');
+  } else {
+    console.error('Unexpected error:', err.code);
+  }
+});
+
+// 导出数据库连接对象
+export default db;
+
