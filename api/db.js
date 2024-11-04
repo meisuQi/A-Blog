b9f5cc7858687b3683333356cd6a9bc8ee42e4d9
meisuQi <2235889780@qq.com>
2024-11-04 21:03:48 +0800

    create PostgreSQL

--- api/db.js
+++ api/db.js
@@ -1,23 +1,55 @@
-import mysql from 'mysql';
-export const db=mysql.createConnection({
-  host:'localhost',
-  user:"root",
-  password:"qms199812230",
-  database:"blog"
-})
+// import mysql from 'mysql';
+// export const db=mysql.createConnection({
+//   host:'localhost',
+//   user:"root",
+//   password:"qms199812230",
+//   database:"blog"
+// })
+
+// // 连接到数据库
+// db.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL:', err);
+//     return;
+//   }
+//   console.log('Connected to MySQL database');
+// });
+
+// // 监听数据库连接错误
+// db.on('error', (err) => {
+//   console.error('MySQL error occurred:', err);
+//   if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//     console.error('Database connection was closed.');
+//   } else {
+//     console.error('Unexpected error:', err.code);
+//   }
+// });
+
+// // 导出数据库连接对象
+// export default db;
+import { Pool } from 'pg';
+
+// 创建 PostgreSQL 连接池
+const pool = new Pool({
+  host: 'dpg-csjo201u0jms73b3n1k0-a',  // Render 提供的主机名
+  port: 5432,                         // 默认 PostgreSQL 端口
+  user: 'blog_zoe7_user',             // Render 提供的用户名
+  password: 'UucjRVNgu8NgEuECeTKBy7OCPJSIYTc8',  // Render 提供的密码
+  database: 'blog_zoe7',              // Render 提供的数据库名称
+});
 
 // 连接到数据库
-db.connect((err) => {
+pool.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error('Error connecting to PostgreSQL:', err);
     return;
   }
-  console.log('Connected to MySQL database');
+  console.log('Connected to PostgreSQL database');
 });
 
 // 监听数据库连接错误
-db.on('error', (err) => {
-  console.error('MySQL error occurred:', err);
+pool.on('error', (err) => {
+  console.error('PostgreSQL error occurred:', err);
   if (err.code === 'PROTOCOL_CONNECTION_LOST') {
     console.error('Database connection was closed.');
   } else {
@@ -26,5 +58,5 @@ db.on('error', (err) => {
 });
 
 // 导出数据库连接对象
-export default db;
+export default pool;
 
